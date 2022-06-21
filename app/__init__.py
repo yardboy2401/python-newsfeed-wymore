d2e0a6fe542a74f6179f057806da5f7f37071304
Jeff Wymore <yardboy2401@gmail.com>
2022-06-21 12:41:30 -0700

    added database app context, configured utils/filters and updated template views to use them

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from app.db import Base, init_db
 from app.routes import home, dashboard
+from app.utils import filters
+
 
 def create_app(test_config=None):
     # setup app config
@@ -16,5 +19,12 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    # jinja filter registration
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
+
+    init_db(app)
 
     return app
